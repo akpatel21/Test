e0b769bcdf5227d1a27f69fb9704572b9d9fabab
akpatel21 <43002673+akpatel21@users.noreply.github.com>
2018-11-29 08:37:41 -0500

    Add files via upload

--- src/Test.java
+++ src/Test.java
@@ -9,7 +9,14 @@ public class Test
 
 	public static void main(String[] args) 
 	{
-
+		int iDNum = 12345, iDNum2= 26810;
+		String name = "Mr. Ellis", name2 = "Mrs. Ellis";
+		
+		System.out.printf("Name: %-20s ID: %,12d", name2, iDNum2);
+		System.out.printf("\nName: %-20s ID: %012d", name, iDNum);
+		int x = 10;
+		System.out.println("\n" + x++);
+		System.out.print(x);
 	}
 
 }
